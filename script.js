14dc05b76a66355df22cfe41eee06a76dbf61d8d
Fakhriddin <fakhmirakov1998@gmail.com>
2023-04-28 15:55:10 +0500

    inputs

--- script.js
+++ script.js
@@ -1,18 +1,93 @@
-const buttons = document.querySelectorAll('.btn')
-const contents = document.querySelectorAll('.content_item')
+// Hearder
+const allInputs = document.querySelectorAll('.all_inputs')
+const errors = document.querySelector('.error')
+const successes = document.querySelector('.success')
+errors.innerHTML = 0
+successes.innerHTML = 0
 
-// let index = 0
+// Form
+const form = document.forms.form
+const needToFill = document.querySelectorAll('.need-to-fill')
+const formInputs = document.querySelectorAll('.input_box')
 
-showTabs(index = 0)
-function showTabs(index) {
-  contents.forEach(content => content.style.display = 'none')
-  contents[index].style.display = 'block'
+allInputs.forEach(el => {
+  el.innerHTML = formInputs.length
+})
+
+
+form.onsubmit = (event) => {
+  event.preventDefault()
+
+  let arr = []
+
+  formInputs.forEach(el => {
+    let elements = el.querySelectorAll('.input')
+    elements.forEach(inp => {
+      if (!inp.value.length) {
+        arr.push(inp)
+        errors.innerHTML = arr.length
+        successes.innerHTML = formInputs.length - arr.length
+      }
+    })
+  })
+
+  
+
+  let err = ''
+
+  needToFill.forEach(input_box => {
+    let inputs = input_box.querySelectorAll('input')
+    inputs.forEach(input => {
+      input.style.border = '1px solid green'
+
+      if (input.value.length === 0) {
+        input_box.classList.add('need_fill')
+        input.style.border = '1px solid red'
+        err += ` ${input.name}`
+      } else{
+        input_box.classList.remove('need_fill')
+      }
+    })
+
+  })
+
+  if (!err) {
+    saveChanges()
+  }
 }
 
-buttons.forEach((btn, index) => {
-  btn.onclick = () => {
-    buttons.forEach(btn => btn.classList.remove('active'))
-    btn.classList.add('active')
-    showTabs(index)
-  }
+
+function saveChanges() {
+  let changes = {}
+  let db = new FormData(form)
+  db.forEach((value, key) => {
+    changes[key] = value
+  })
+
+  console.log(changes);
+}
+
+const patterns = {
+  name: /^[a-z ,.'-]+$/i,
+  email: /^\S+@\S+\.\S+$/,
+  password: /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$/,
+  age: /^\d+$/,
+  text: /^[a-z0-9_ '.!?""-]{1,50}$/i
+}
+
+formInputs.forEach(inp => {
+  let inputs = inp.querySelectorAll('input')
+  inputs.forEach(input => {
+    input.onkeyup = () => {
+      validate(patterns[input.name], input)
+    }
+  })
 })
+
+function validate(regex, field) {
+  if(regex.test(field.value)) {
+    field.style.border = '1px solid green'
+  } else{
+    field.style.border = '1px solid red'
+  }
+}
