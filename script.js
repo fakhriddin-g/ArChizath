82b59bcb2930c5ce2a3d4d2ad875aaea5c34048b
Fakhriddin <fakhmirakov1998@gmail.com>
2023-05-03 18:55:37 +0500

    product card ready

--- script.js
+++ script.js
@@ -1,18 +1,47 @@
-const buttons = document.querySelectorAll('.btn')
-const contents = document.querySelectorAll('.content_item')
+import arr from './module/db.js'
 
-// let index = 0
+let container = document.querySelector('.container')
 
-showTabs(index = 0)
-function showTabs(index) {
-  contents.forEach(content => content.style.display = 'none')
-  contents[index].style.display = 'block'
+let icons = ['price', 'rate', 'count']
+
+for (const item of arr) {
+  let item_div = document.createElement('div')
+  let img = document.createElement('img')
+  let description = document.createElement('div')
+  let h3 = document.createElement('h3')
+  let p = document.createElement('p')
+  let row = document.createElement('div')
+  let button = document.createElement('button')
+
+  for (const icon of icons) {
+    let feature = document.createElement('div')
+    let img = document.createElement('img')
+    let span = document.createElement('span')
+
+    feature.classList.add('feature')
+
+    img.src = './public/icon/' + icon + '.svg'
+    span.innerHTML = typeof(item[icon]) !== 'undefined' ? item[icon] : item.rating[icon]
+
+    feature.append(img, span)
+    row.append(feature)
+
+  }
+
+  item_div.classList.add('item')
+  img.classList.add('main-img')
+  description.classList.add('description')
+  row.classList.add('row')
+
+  h3.innerHTML = item.title.slice(0, 20)
+  p.innerHTML = item.description.slice(0, 59).toLowerCase()
+  img.src = item.image
+  button.innerHTML = 'В избранное'
+
+  item_div.append(img, description)
+  description.append(h3, p, row, button)
+
+  container.append(item_div)
+
 }
 
-buttons.forEach((btn, index) => {
-  btn.onclick = () => {
-    buttons.forEach(btn => btn.classList.remove('active'))
-    btn.classList.add('active')
-    showTabs(index)
-  }
-})
