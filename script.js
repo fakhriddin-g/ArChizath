fbaf0516514f7dd38cd4aea192c1f5c1bc60dd8d
Fakhriddin <fakhmirakov1998@gmail.com>
2023-05-05 02:34:18 +0500

    modfied js file: showFive, showAll, cart amount

--- script.js
+++ script.js
@@ -1,96 +1,96 @@
-import arr from './module/db.js'
+import product from './module/db.js'
 
 
 
-
-// Item
 let container = document.querySelector('.container')
-
-let icons = ['price', 'rate', 'count']
-
-for (const item of arr) {
-  let item_div = document.createElement('div')
-  let img = document.createElement('img')
-  let description = document.createElement('div')
-  let h3 = document.createElement('h3')
-  let p = document.createElement('p')
-  let row = document.createElement('div')
-  let button = document.createElement('button')
-
-  for (const icon of icons) {
-    let feature = document.createElement('div')
-    let img = document.createElement('img')
-    let span = document.createElement('span')
-
-    feature.classList.add('feature')
-
-    img.src = './public/icon/' + icon + '.svg'
-    span.innerHTML = typeof(item[icon]) !== 'undefined' ? item[icon] : item.rating[icon]
-
-    feature.append(img, span)
-    row.append(feature)
-
-  }
-
-  item_div.classList.add('item')
-  img.classList.add('main-img')
-  description.classList.add('description')
-  row.classList.add('row')
-
-  h3.innerHTML = item.title.slice(0, 20)
-  p.innerHTML = item.description.slice(0, 59).toLowerCase()
-  img.src = item.image
-  button.innerHTML = 'В избранное'
-
-  item_div.append(img, description)
-  description.append(h3, p, row, button)
-
-  container.append(item_div)
-
-}
-
-
-let btn = document.querySelectorAll('.description button')
-
+// ================================
 let amount = document.querySelector('.center span')
-let show = document.querySelector('.show-5-card')
-let show_all = document.querySelector('.show-all')
-amount.innerHTML = 0
-
-show.onclick = () => {
-  arr.splice(5, 20)
-  console.log(arr);
-}
-
-btn.forEach(item => {
-  let item_add = item.classList == 'add-to-mark'
-  item.onclick = () => {
-    if(!item_add) {
-      item.classList.add('add-to-mark')
-      item.innerHTML = 'Добавлено'
-      amount.innerHTML++
-      item_add = true
-    } else if (item_add) {
-      item.classList.remove('add-to-mark')
-      item.innerHTML = 'В избранное'
-      amount.innerHTML--
-      item_add = false
-    }
-  }
-})
-
-let shoppedBtn = document.querySelector('.shopped-btn')
+let data_showFive = document.querySelector('button[data-show-five]')
+let data_showAll = document.querySelector('button[data-show-all]')
+let data_cart = document.querySelector('button[data-cart]')
 let shopped = document.querySelector('.shopped')
 let bg = document.querySelector('.bg')
 
-shoppedBtn.onclick = () => {
+
+let icons = ['price', 'rate', 'count']
+let cart = []
+
+reloadCard(product, container)
+
+function reloadCard(arr, place) {
+  place.innerHTML = ''
+  amount.innerHTML = cart.length
+  
+  for (const item of arr) {
+    let item_div = document.createElement('div')
+    let img = document.createElement('img')
+    let description = document.createElement('div')
+    let h3 = document.createElement('h3')
+    let p = document.createElement('p')
+    let row = document.createElement('div')
+    let button = document.createElement('button')
+  
+    for (const icon of icons) {
+      let feature = document.createElement('div')
+      let img = document.createElement('img')
+      let span = document.createElement('span')
+  
+      feature.classList.add('feature')
+  
+      img.src = './public/icon/' + icon + '.svg'
+      span.innerHTML = typeof(item[icon]) !== 'undefined' ? item[icon] : item.rating[icon]
+  
+      feature.append(img, span)
+      row.append(feature)
+    }
+  
+    item_div.classList.add('item')
+    img.classList.add('main-img')
+    description.classList.add('description')
+    row.classList.add('row')
+  
+    h3.innerHTML = item.title.slice(0, 20)
+    p.innerHTML = item.description.slice(0, 59).toLowerCase()
+    img.src = item.image
+    button.innerHTML = 'В избранное'
+  
+    item_div.append(img, description)
+    description.append(h3, p, row, button)
+  
+    place.append(item_div)
+  
+    button.onclick = () => {
+      if(cart.includes(item.id)) {
+        cart = cart.filter(id => id !== item.id)
+        button.classList.remove('add-to-mark')
+        button.innerHTML = 'В избранное'
+      } else {
+        cart.push(item.id)
+        button.classList.add('add-to-mark')
+        button.innerHTML = 'Добавлено'
+      }
+
+      amount.innerHTML = cart.length
+      console.log(cart);
+    }
+  }
+}
+
+// ===========================================
+data_showFive.onclick = () => {
+  reloadCard(product.slice(0,5), container)
+}
+data_showAll.onclick = () => {
+  reloadCard(product, container)
+}
+
+// ===========================================
+data_cart.onclick = () => {
   shopped.style.right = '0'
   bg.style.left = '0'
   bg.style.scale = '1'
-  body.style.scroll(0)
 }
 bg.onclick = () => {
   shopped.style.right = '-33%'
   bg.style.scale = '0'
-  // bg.style.left = '-100%'
 }
