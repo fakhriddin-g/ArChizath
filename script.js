97020dda547e0d034e44fc13890499bd697ff40c
Fakhriddin <fakhmirakov1998@gmail.com>
2023-05-04 16:37:48 +0500

    js

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 import arr from './module/db.js'
 
+
+
+
+// Item
 let container = document.querySelector('.container')
 
 let icons = ['price', 'rate', 'count']
@@ -45,3 +49,48 @@ for (const item of arr) {
 
 }
 
+
+let btn = document.querySelectorAll('.description button')
+
+let amount = document.querySelector('.center span')
+let show = document.querySelector('.show-5-card')
+let show_all = document.querySelector('.show-all')
+amount.innerHTML = 0
+
+show.onclick = () => {
+  arr.splice(5, 20)
+  console.log(arr);
+}
+
+btn.forEach(item => {
+  let item_add = item.classList == 'add-to-mark'
+  item.onclick = () => {
+    if(!item_add) {
+      item.classList.add('add-to-mark')
+      item.innerHTML = 'Добавлено'
+      amount.innerHTML++
+      item_add = true
+    } else if (item_add) {
+      item.classList.remove('add-to-mark')
+      item.innerHTML = 'В избранное'
+      amount.innerHTML--
+      item_add = false
+    }
+  }
+})
+
+let shoppedBtn = document.querySelector('.shopped-btn')
+let shopped = document.querySelector('.shopped')
+let bg = document.querySelector('.bg')
+
+shoppedBtn.onclick = () => {
+  shopped.style.right = '0'
+  bg.style.left = '0'
+  bg.style.scale = '1'
+  body.style.scroll(0)
+}
+bg.onclick = () => {
+  shopped.style.right = '-33%'
+  bg.style.scale = '0'
+  // bg.style.left = '-100%'
+}
