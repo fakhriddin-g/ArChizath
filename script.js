195fe8c9fceb787658da24a2ad12ae6a1de06b68
Fakhriddin <fakhmirakov1998@gmail.com>
2023-05-09 15:53:57 +0500

    some edit in js file

--- script.js
+++ script.js
@@ -4,20 +4,25 @@ import products from './module/db.js'
 
 let container = document.querySelector('.container')
 let shopped = document.querySelector('.shopped')
-let cart_box_scroll = document.querySelector('.cart-box-scroll')
+let cartBoxScroll = document.querySelector('.cart-box-scroll')
 // ================================
 let amount = document.querySelector('.center span')
-let data_showFive = document.querySelector('button[data-show-five]')
-let data_showAll = document.querySelector('button[data-show-all]')
-let data_cart = document.querySelector('button[data-cart]')
+let dataShowFive = document.querySelector('button[data-show-five]')
+let dataShowAll = document.querySelector('button[data-show-all]')
+let dataCart = document.querySelector('button[data-cart]')
 let bg = document.querySelector('.bg')
 // ================================
-let total_money = document.querySelector('.total_money')
+let totalMoney = document.querySelector('.total_money')
+// =========================================================
+// =========================================================
+let orderBtn = document.querySelector('.order')
+let modal = document.querySelector('.modal')
+let inputs = document.querySelectorAll('form input')
+let modalBtn = document.querySelector('#order')
 
 let icons = ['price', 'rate', 'count']
 let cart = []
 
-reload(cart, cart_box_scroll)
 reloadCard(products, container)
 
 function reloadCard(arr, place) {
@@ -26,7 +31,7 @@ function reloadCard(arr, place) {
   
   for (const item of arr) {
     // Main Item
-    let item_div = document.createElement('div')
+    let itemDiv = document.createElement('div')
     let img = document.createElement('img')
     let description = document.createElement('div')
     let h3 = document.createElement('h3')
@@ -48,7 +53,7 @@ function reloadCard(arr, place) {
       row.append(feature)
     }
   
-    item_div.classList.add('item')
+    itemDiv.classList.add('item')
     img.classList.add('main-img')
     description.classList.add('description')
     row.classList.add('row')
@@ -56,142 +61,183 @@ function reloadCard(arr, place) {
     h3.innerHTML = item.title.slice(0, 20)
     p.innerHTML = item.description.slice(0, 59).toLowerCase()
     img.src = item.image
-    button.innerHTML = 'В избранное'
+    // button.innerHTML = 'В избранное'
   
-    item_div.append(img, description)
+    itemDiv.append(img, description)
     description.append(h3, p, row, button)
-    place.append(item_div)
+    place.append(itemDiv)
 
-    
+    if (cart.includes(item.id)) {
+      button.innerHTML = 'Добавлено'
+      button.classList.add('add-to-mark')
+    } else{
+      button.classList.remove('add-to-mark')
+      button.innerHTML = 'В избранное'
+    }
     
     button.onclick = () => {
-      if(cart.includes(item)) {
-        cart = cart.filter(id => id !== item)
+      if(cart.includes(item.id)) {
+        cart = cart.filter(id => id !== item.id)
         button.classList.remove('add-to-mark')
         button.innerHTML = 'В избранное'
       } else {
-        cart.push(item)
+        cart.push(item.id)
         button.classList.add('add-to-mark')
         button.innerHTML = 'Добавлено'
-        total_money += item.price
       }
-      // total_money.innerHTML = total_money
-
-      if (cart.length == 0) {
-        total_money.innerHTML = 0 + ' $'
-      }
-      
-      reload(cart, cart_box_scroll)
+            
       amount.innerHTML = cart.length
+      reload()
     }
   }
 }
 
-function reload(arr, place) {
-  place.innerHTML = ''
+function reload() {
+  cartBoxScroll.innerHTML = ''
+  let temp = []
+  let allPrices = 0
+  let qtCount = 0
 
-  for (const element of arr) {
+  for (const item of products) {
+    for (const id of cart) {
+      if (item.id === id) {
+        temp.push({
+          ...item,
+          qt: 1
+        })
+      }
+    }
+  }
+
+  for (const item of temp) {
     // Shopped Item
-    let cart_item = document.createElement('div')
-    let item_checkbox = document.createElement('input')
-    let item_img = document.createElement('img')
+    let cartItem = document.createElement('div')
+    let itemCheckbox = document.createElement('input')
+    let itemImg = document.createElement('img')
     let product = document.createElement('div')
-    let item_title = document.createElement('div')
-    let item_title_h3 = document.createElement('h3')
-    let item_title_button = document.createElement('button')
-    let item_title_button_img = document.createElement('img')
-    let item_title_button_span = document.createElement('span')
-    let item_info = document.createElement('div')
-    let product_info = document.createElement('div')
-    let item_color = document.createElement('span')
-    let item_counter = document.createElement('div')
-    let item_decrease = document.createElement('span')
-    let item_input = document.createElement('input')
-    let item_increase = document.createElement('span')
-    let item_price = document.createElement('div')
-    let discount_price = document.createElement('div')
+    let itemTitle = document.createElement('div')
+    let itemTitleH3 = document.createElement('h3')
+    let itemTitleButton = document.createElement('button')
+    let itemTitleButtonImg = document.createElement('img')
+    let itemTitleButtonSpan = document.createElement('span')
+    let itemInfo = document.createElement('div')
+    let productInfo = document.createElement('div')
+    let itemColor = document.createElement('span')
+    let itemCounter = document.createElement('div')
+    let itemDecrease = document.createElement('span')
+    let itemInput = document.createElement('input')
+    let itemIncrease = document.createElement('span')
+    let itemPrice = document.createElement('div')
+    let discountPrice = document.createElement('div')
   
   
-    cart_item.classList.add('cart-item')
-    item_img.classList.add('item-img')
+    cartItem.classList.add('cart-item')
+    itemImg.classList.add('item-img')
     product.classList.add('product')
-    item_title.classList.add('item-title')
-    item_info.classList.add('item-info')
-    product_info.classList.add('product-info')
-    item_color.classList.add('item-color')
-    item_counter.classList.add('item-counter')
-    item_decrease.classList.add('item-derease')
-    item_increase.classList.add('item-increase')
-    item_price.classList.add('item-price')
-    discount_price.classList.add('discount-price')
+    itemTitle.classList.add('item-title')
+    itemInfo.classList.add('item-info')
+    productInfo.classList.add('product-info')
+    itemColor.classList.add('item-color')
+    itemCounter.classList.add('item-counter')
+    itemDecrease.classList.add('item-derease')
+    itemIncrease.classList.add('item-increase')
+    itemPrice.classList.add('item-price')
+    discountPrice.classList.add('discount-price')
   
-    item_checkbox.type = 'checkbox'
-    item_checkbox.checked = true
-    item_img.src = element.image
-    item_title_h3.innerHTML = element.title.slice(0,28)
-    item_title_button_img.src = './public/icon/count.svg'
-    item_title_button_span.innerHTML = 'Удалить'
-    item_color.innerHTML = ''
-    item_decrease.innerHTML = '-'
-    item_input.value = '1'
-    item_increase.innerHTML = '+'
-    discount_price.innerHTML = element.price + ' $'
+    itemCheckbox.type = 'checkbox'
+    itemCheckbox.checked = true
+    itemImg.src = item.image
+    itemTitleH3.innerHTML = item.title.slice(0,28)
+    itemTitleButtonImg.src = './public/icon/count.svg'
+    itemTitleButtonSpan.innerHTML = 'Удалить'
+    itemColor.innerHTML = ''
+    itemDecrease.innerHTML = '-'
+    itemInput.value = '1'
+    itemIncrease.innerHTML = '+'
+    discountPrice.innerHTML = item.price * item.qt + ' $'
   
   
-    product.append(item_title, item_info, item_counter, item_price)
-    item_title.append(item_title_h3, item_title_button)
-    item_title_button.append(item_title_button_img, item_title_button_span)
-    item_info.append(product_info, item_counter, item_price)
-    product_info.append(item_color)
-    item_counter.append(item_decrease, item_input, item_increase)
-    item_price.append(discount_price)
+    product.append(itemTitle, itemInfo, itemCounter, itemPrice)
+    itemTitle.append(itemTitleH3, itemTitleButton)
+    itemTitleButton.append(itemTitleButtonImg, itemTitleButtonSpan)
+    itemInfo.append(productInfo, itemCounter, itemPrice)
+    productInfo.append(itemColor)
+    itemCounter.append(itemDecrease, itemInput, itemIncrease)
+    itemPrice.append(discountPrice)
+    cartItem.append(itemCheckbox, itemImg, product)
+    cartBoxScroll.append(cartItem)
 
-    cart_item.append(item_checkbox, item_img, product)
-    place.append(cart_item)
-
-
-    item_decrease.onclick = () => {
-      if (item_input.value > 1) {
-        item_input.value--
-        discount_price.innerHTML = ((item_input.value * element.price).toFixed(2)) + ' $'
-        total_money.innerHTML = discount_price.innerHTML
-      } else{
-        item_input.value
-      }
-    }
-    item_increase.onclick = () => {
-      if (item_input.value < element.rating.count) {
-        item_input.value++
-        discount_price.innerHTML = ((item_input.value * element.price).toFixed(2)) + ' $'
-        total_money.innerHTML = discount_price.innerHTML
-      }else{
-        item_input.value = element.rating.count
-      }
-    }
-
+    allPrices += item.price * item.qt
     
+    itemCheckbox.onchange = () => {
+      if (itemCheckbox.checked) {
+        allPrices += item.price * item.qt
+      } else {
+        allPrices -= item.price * item.qt
+      }
 
-    item_title_button.onclick = () => {
-      cart_item.remove()
+      totalMoney.innerHTML = `${allPrices.toLocaleString('tj-TJ')} $`
     }
 
+    itemDecrease.onclick = () => {
+      if (itemInput.value > 1) {
+        itemInput.value--
+        item.qt = itemInput.value
+        allPrices += item.price * item.qt
+      }
+    }
+
+    itemIncrease.onclick = () => {
+      if (itemInput.value < item.rating.count) {
+        itemInput.value++
+        allPrices += item.price * item.qt
+        discountPrice.innerHTML = allPrices
+        totalMoney.innerHTML = `${allPrices.toLocaleString('tj-TJ')} $`
+      }
+    }
+
+    itemTitleButton.onclick = () => {
+      let sure = confirm('R U Sure')
+
+      if (sure) {
+        cart = cart.filter(id => id !== item.id)
+        reload()
+        reloadCard(products, container)
+      }
+    }
+
+    form.onsubmit = (event) => {
+      event.preventDefault()
+    
+      modalBtn.onclick = () => {
+        saveChanges(temp)
+      }
+
+    }
+    
+    
+    reloadCard(products, container)
   }
+  
+  totalMoney.innerHTML = `${allPrices.toLocaleString('tj-TJ')} $`
 }
 
+orderBtn.onclick = () => {
+  modal.style.display = 'flex'
 
-
+}
 
 // ===========================================
-data_showFive.onclick = () => {
+dataShowFive.onclick = () => {
   reloadCard(products.slice(0,5), container)
 }
-data_showAll.onclick = () => {
+dataShowAll.onclick = () => {
   reloadCard(products, container)
 }
 
 // ===========================================
 // shtorka
-data_cart.onclick = () => {
+dataCart.onclick = () => {
   shopped.style.right = '0'
   shopped.style.scale = '1'
   bg.style.left = '0'
@@ -204,4 +250,20 @@ bg.onclick = () => {
   bg.style.scale = '0'
   bg.style.left = '-100%'
   document.body.style.overflow = "scroll"
+  modal.style.display = 'none'
 }
+
+
+
+function saveChanges(temp) {
+  let changes = {buyer:{}}
+  let db = new FormData(form)
+  db.forEach((value, key) => {
+    changes.buyer[key] = value
+  })
+  changes.goods = temp
+
+  console.log(changes);
+}
+
+
