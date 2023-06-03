311d547f9c74e61439c93c4037f80f9c47debf2f
Fakhriddin <fakhmirakov1998@gmail.com>
2023-06-03 15:02:54 +0500

    Patch and Delete

--- script.js
+++ script.js
@@ -1,43 +1,56 @@
-let baseURL = "http://localhost:9999"
-
-const getAllData = () => {
-  fetch(baseURL + "/todos")
-    .then(res => res.json())
-    .then(data => reload(data))
-}
-
-getAllData()
 
 let todoText = document.querySelector('#todo-text')
 let addTodo = document.querySelector('#add-todo')
 let form = document.forms.form
 
+let modal = document.querySelector('.bg')
+
 
 let container = document.querySelector('.container')
 
+let baseURL = "http://localhost:9999"
+const getAllData = async () => {
+
+  try {
+    const res = await fetch(baseURL + "/todos")
+
+    if (res.status === 200 || res.status === 201) {
+      const data = await res.json()
+      reload(data)
+    }
+  } catch (e) {
+    alert('Connection error')
+  }
+}
+
+getAllData()
 // let todos = []
 
 function reload(arr) {
   container.innerHTML = ''
-  
+
   for (const item of arr) {
     let todoBox = document.createElement('div')
     let todoTitle = document.createElement('div')
     let h2 = document.createElement('h2')
     let span = document.createElement('span')
-    let img = document.createElement('img')
-    
+    let edite = document.createElement('p')
+    let del = document.createElement('p')
+
     todoBox.classList.add('todo-box')
     todoTitle.classList.add('todo-title')
     h2.classList.add('title')
     span.classList.add('hour')
-    
+    edite.classList.add('edite')
+    del.classList.add('delete')
+
     h2.innerHTML = item.task
     span.innerHTML = item.time
-    img.src = './public/icon/price.svg'
-    
-    todoBox.append(todoTitle, img)
+    edite.innerHTML = 'edite'
+    del.innerHTML = 'delete'
+
     todoTitle.append(h2, span)
+    todoBox.append(todoTitle, edite, del)
     container.append(todoBox)
 
     item.isDone = false
@@ -53,8 +66,30 @@ function reload(arr) {
       }
     }
 
-    img.onclick = () => {
-      console.log(todos);
+    del.onclick = async () => {
+      const res = await fetch(baseURL + "/todos/" + item.id, {
+        method: "delete"
+      })
+      if (res.status === 200 || res.status === 201) {
+        todoBox.remove()
+      }
+    }
+
+    edite.onclick =async () => {
+      try {
+        const res = await fetch(baseURL + "/todos/" + item.id, {
+          method: "PATCH",
+          body: JSON.stringify({task: prompt(item.task)}),
+          headers: {
+            "Content-Type": "application/json"
+          }
+        })
+        if (res.status === 200 || res.status === 201) {
+          getAllData()
+        }
+      } catch (e) {
+        alert('Connection error')
+      }
     }
   }
 }
@@ -65,7 +100,8 @@ form.onsubmit = (event) => {
   todoText.style.border = '1px solid green'
   if (todoText.value) {
     save()
-  } else{
+    form.reset()
+  } else {
     todoText.style.border = '1px solid red'
   }
 }
@@ -86,17 +122,19 @@ function save() {
   createNewTask(task)
 }
 
-const createNewTask = (body) => {
-  fetch(baseURL + "/todos", {
-    method: "post",
-    body: JSON.stringify(body),
-    headers: {
-      "Content-Type": "application/json"
-    }
-  })
-  .then(res => {
+const createNewTask = async (body) => {
+  try {
+    const res = await fetch(baseURL + "/todos", {
+      method: "post",
+      body: JSON.stringify(body),
+      headers: {
+        "Content-Type": "application/json"
+      }
+    })
     if (res.status === 200 || res.status === 201) {
       getAllData()
     }
-  })
+  } catch (e) {
+    alert('Connection error')
+  }
 }
